071c9ecdf581aa971fc02ddeeb6029e2a5becf2d
BOYAKIRAN321 <boyakiran824@gmail.com>
2026-09-05 22:56:29 +0530

    Update app.py

--- student_data/backend/app.py
+++ student_data/backend/app.py
@@ -1,30 +1,34 @@
 from flask import Flask, request, jsonify, send_from_directory
 from backend.rag_pipeline import run_rag
 import os
+import traceback
 
 app = Flask(__name__, static_folder="../frontend", static_url_path="")
-# Serve index.html
+
 @app.route("/")
 def serve_index():
     return send_from_directory(app.static_folder, "index.html")
 
-# Serve other static files (CSS, JS)
 @app.route("/<path:path>")
 def serve_static(path):
     return send_from_directory(app.static_folder, path)
 
 @app.route("/ask", methods=["POST"])
 def ask():
-    data = request.get_json()
-    query = data.get("query")
-
-    if not query:
-        return jsonify({"error": "Query required"}), 400
-
-    answer = run_rag(query)
-    print("heyy hii",answer)
-
-    return jsonify({"answer": answer})
+    try:
+        data = request.get_json()
+        query = data.get("query")
+        if not query:
+            return jsonify({"error": "Query required"}), 400
+        
+        print(f"Query: {query}")
+        answer = run_rag(query)
+        print(f"Answer: {answer[:100]}")
+        return jsonify({"answer": answer})
+    except Exception as e:
+        print(f"ERROR in /ask: {e}")
+        traceback.print_exc()
+        return jsonify({"answer": f"Server Error: {str(e)}"}), 500
 
 if __name__ == "__main__":
     app.run(debug=True)
